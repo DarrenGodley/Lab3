20fcd684cf95f5ebd3932c2e63fe03296fd8ae60
Darren Godley <darren.godley@students.ittralee.ie>
2018-10-08 12:59:37 +0100

    myPowRecursive

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,14 +2,18 @@
 // Recursive factorial method
 
 public class Factorial {
+
+   public static void main(String[] args) {
+      fact(10);
+   }
    
    // Recursive definition of method factorial
    public static int fact( int n )
-   {      
+   {
       if ( n == 1 || n == 0 )  // base case
          return 1;
       else {
-         int result1 = fact(n-1); 
+         int result1 = fact(n-1);
          int result2 = n * result1;
          return result2;
          //return n * fact(n-1);
--- src/Java.java
+++ src/Java.java
@@ -1,9 +1,27 @@
 public class Java {
-    public static double myPow(double a, int b){
-        if (b == 0){
+
+    public static void main(String[] args) {
+        int res = Java.myPowRecursive(2,5);
+        System.out.println("Result of 2 to the power of 5 is " + res);
+    }
+
+    //Write a recursive version of pow function i.e.
+    //public static int myPow(int x, int y)  where y an integer >= 0 // Returns the value of the first argument raised to the power of the second argument.
+
+    public static int myPowRecursive(int x, int y) {
+        int result = 0;
+
+        if (y == 0){
             return 1;
         }
+
         else
-            return a * myPow(a,b-1);
+            return x * myPowRecursive(x, y-1);
     }
+
+    //Write an iterative version of myPow method. Profile both iterative and recursive version. Which is more efficient in terms of CPU time?
+    public static int myPowIterative(int x, int y){
+        return 0;
+    }
+
 }
